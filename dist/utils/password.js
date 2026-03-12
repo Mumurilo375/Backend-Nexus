cbba797952da30afd7ca2780a5a4439521e10b5e
Mumurilo375 <murilinho.pequeno@gmail.com>
2026-03-11 23:28:10 -0300

    chore: adiciona dist ao gitignore

--- dist/utils/password.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hashPassword = hashPassword;
-const crypto_1 = require("crypto");
-function hashPassword(password) {
-    const salt = (0, crypto_1.randomBytes)(16).toString("hex");
-    const hash = (0, crypto_1.scryptSync)(password, salt, 64).toString("hex");
-    return `scrypt:${salt}:${hash}`;
-}
