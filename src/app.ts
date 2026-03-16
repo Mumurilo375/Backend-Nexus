6a0a43eeb654f75f5aa4b0eef36bca308f8ea3f6
Mumurilo375 <murilinho.pequeno@gmail.com>
2026-03-16 18:01:24 -0300

    Instala e configura Cors para integracao com frontend

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import { errorMiddleware, notFoundMiddleware } from "./middlewares/error.middleware";
 import routes from "./routes";
 
 const app = express();
 
+app.use(
+	cors({
+		origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+	})
+);
 app.use(express.json());
 app.use(routes);
 app.use(notFoundMiddleware);
