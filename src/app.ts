46077b9772bc5b5db55eaaed82879bb77fc6a34c
Mumurilo375 <murilinho.pequeno@gmail.com>
2026-03-18 21:08:32 -0300

    Reduz bugs no localhost

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+		origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
 		methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 	})
 );
