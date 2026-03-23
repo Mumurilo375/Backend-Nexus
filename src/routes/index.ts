50356fea60d501203e08a1f5f67ea16ff65d29fa
Mumurilo375 <murilinho.pequeno@gmail.com>
2026-03-22 23:03:20 -0300

    registra novas rotas de modulos no index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,22 @@ import authRouter from "./auth.routes";
 import cartRouter from "./cart.routes";
 import categoriesRouter from "./categories.routes";
 import checkoutRouter from "./checkout.routes";
+import deliveredKeysRouter from "./delivered-keys.routes";
+import gameImagesRouter from "./game-images.routes";
+import gameKeysRouter from "./game-keys.routes";
+import gameTagsRouter from "./game-tags.routes";
 import gamesRouter from "./games.routes";
 import historyRouter from "./history.routes";
 import libraryRouter from "./library.routes";
 import orderItemsRouter from "./order-items.routes";
 import ordersRouter from "./orders.routes";
 import platformsRouter from "./platforms.routes";
+import promotionsRouter from "./promotions.routes";
+import reviewVotesRouter from "./review-votes.routes";
+import reviewsRouter from "./reviews.routes";
 import usersRouter from "./users.routes";
 import wishlistRouter from "./wishlist.routes";
+import listingsRouter from "./listings.routes";
 
 const router = Router();
 
@@ -38,5 +46,13 @@ router.use("/orders", ordersRouter);
 router.use("/order-items", orderItemsRouter);
 router.use("/library", libraryRouter);
 router.use("/history", historyRouter);
+router.use("/delivered-keys", deliveredKeysRouter);
+router.use("/reviews", reviewsRouter);
+router.use("/review-votes", reviewVotesRouter);
+router.use("/game-keys", gameKeysRouter);
+router.use("/listings", listingsRouter);
+router.use("/promotions", promotionsRouter);
+router.use("/game-images", gameImagesRouter);
+router.use("/game-tags", gameTagsRouter);
 
 export default router;
